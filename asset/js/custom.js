25a4174fd9f89d6b2325e2396294414df05c871f
jiyoonjang <rchel02@naver.com>
2022-07-16 19:34:29 +0900

    수업후 피드백1

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -1,48 +1,48 @@
 $(function () {
-      /**
-     * 헤더 슬라이드
-     *
-     * @version 1.0.0
-     * @since 2022-07-04
-     * @author jy
-     */
-  var swiper = new Swiper(".top-slide", {
-    slidesPerView: 1,
-    loop: true,
-    autoplay: {
-      delay: 2000,
-    },
-    direction: "vertical",
-  });
+  /**
+ * 헤더 슬라이드
+ *
+ * @version 1.0.0
+ * @since 2022-07-04
+ * @author jy
+ */
+var swiper = new Swiper(".top-slide", {
+slidesPerView: 1,
+loop: true,
+autoplay: {
+  delay: 2000,
+},
+direction: "vertical",
+});
 
-      /**
-     * 메인 슬라이드
-     *
-     * @version 1.0.0
-     * @since 2022-07-04
-     * @author jy
-     */
-  var swiper = new Swiper(".main-slide", {
-    slidesPerView: 1,
-    loop: true,
-    autoplay: {
-      delay: 4000,
-    },
-    effect: 'fade',
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true
-    },
-  });
+  /**
+ * 메인 슬라이드
+ *
+ * @version 1.0.0
+ * @since 2022-07-04
+ * @author jy
+ */
+var swiper = new Swiper(".main-slide", {
+slidesPerView: 1,
+loop: true,
+autoplay: {
+  delay: 4000,
+},
+effect: 'fade',
+pagination: {
+  el: ".swiper-pagination",
+  clickable: true
+},
+});
 
 
-    /**
-     * 슬라이드 나타나면 글자도 나타나게
-     *
-     * @version 1.0.0
-     * @since 2022-07-06
-     * @author jy
-     */
+/**
+ * 슬라이드 나타나면 글자도 나타나게
+ *
+ * @version 1.0.0
+ * @since 2022-07-06
+ * @author jy
+ */
 
 //   $('.swiper-slide.swiper-slide-visible.swiper-slide-active').click(function () {
 //     idx = $(this).index();
@@ -58,148 +58,139 @@ $(function () {
 
 
 
-      /**
-     * 헤더 색상변환, 고정
-     *
-     * @version 1.0.0
-     * @since 2022-07-05
-     * @author jy
-     */
-  $(window).scroll(function () {
-    const curr = $(window).scrollTop();
-    const target = $('.swiper-pagination').offset().top;
-    if (curr >= target) {
-      $('.menu-area').addClass('fixed')
-    } else {
-      $('.menu-area').removeClass('fixed')
-    }
+  /**
+ * 헤더 색상변환, 고정
+ *
+ * @version 1.0.0
+ * @since 2022-07-05
+ * @author jy
+ */
+$(window).scroll(function () {
+const curr = $(window).scrollTop();
+const target = $('.swiper-pagination').offset().top;
+if (curr >= target) {
+  $('.menu-area').addClass('fixed')
+} else {
+  $('.menu-area').removeClass('fixed')
+}
 
+})
+
+  /**
+ * 예약버튼 누르면 예약모달팝업
+ *
+ * @version 1.0.0
+ * @since 2022-07-05
+ * @author jy
+ */
+$('.btn-reserve').click(function (e) {
+e.preventDefault();
+$('.reserve-pop, .dimmed').addClass('active');
+$('body').addClass('hidden');
+
+})
+$('.btn-close, .dimmed').click(function (e) {
+e.preventDefault();
+$('.reserve-pop, .dimmed').removeClass('active');
+$('body').removeClass('hidden');
+})
+
+  /**
+ * 메뉴
+ *
+ * @version 1.0.0
+ * @since 2022-07-06
+ * @author jy
+ */
+   $(window).scroll(function () {
+   const po = $(window).scrollTop();
+   const circle = $('.main-slide').offset().top;
+   if (circle >= po) {
+     $('.btn-reserve').addClass('circle')
+   } else {
+     $('.btn-reserve').removeClass('circle')
+   }
   })
 
-      /**
-     * 예약버튼 누르면 예약모달팝업
-     *
-     * @version 1.0.0
-     * @since 2022-07-05
-     * @author jy
-     */
-  $('.btn-reserve').click(function (e) {
-    e.preventDefault();
-    $('.reserve-pop, .dimmed').addClass('active');
-    $('body').addClass('hidden');
 
-  })
-  $('.btn-close, .dimmed').click(function (e) {
-    e.preventDefault();
-    $('.reserve-pop, .dimmed').removeClass('active');
-    $('body').removeClass('hidden');
-  })
+menu = gsap.fromTo('.header .gnb-pop .gnb-item',{
+opacity:0,
+yPercent:10
+},{
+opacity:1,
+stagger:0.2,
+yPercent:0
+})
+menu.paused();
 
-      /**
-     * 메뉴
-     *
-     * @version 1.0.0
-     * @since 2022-07-06
-     * @author jy
-     */
-       $(window).scroll(function () {
-       const po = $(window).scrollTop();
-       const circle = $('.main-slide').offset().top;
-       if (circle >= po) {
-         $('.btn-reserve').addClass('circle')
-       } else {
-         $('.btn-reserve').removeClass('circle')
-       }
-      })
+
+$('.btn-menu').click(function (e) {
+e.preventDefault();
+if(!$(this).hasClass('close')){
+  //첫클릭
+  menu.restart();
+  $('.header .btn-reserve').removeClass('circle')
+}else{
+  //닫기눌렀을때
+  $('.header .btn-reserve').addClass('circle')
+}
 
 
 
-  $('.btn-menu').click(function (e) {
-    e.preventDefault();
-    // $('.gnb-pop').toggleClass('active');
 
-    //body에 hidden클래스가 있다면 제거,없다면 추가(스크롤 없앰)
-    if ( $('body').hasClass('hidden')) {
-      $('body').removeClass('hidden');
-    } else {
-      $('body').addClass('hidden');
-    }
-    //메뉴가 만약 고정, 색상변환됐다면 되돌리기
-    if ($('.menu-area').hasClass('fixed')) {
-      $('.menu-area').removeClass('fixed')
-    }
-    
-    // $('.btn-menu').toggleClass('close');
-    //gnb-pop이 active클래스 있다면 삭제, 없다면 추가
-    if ($('.gnb-pop').hasClass('active')) {
-      $('.gnb-pop').removeClass('active')
-
-      const curr = $(window).scrollTop();
-      const target = $('.swiper-pagination').offset().top;
-      if (curr >= target) {
-        $('.menu-area').addClass('fixed')
-      } else {
-        $('.menu-area').removeClass('fixed')
-      }
-      // menu.reverse()
-    } else {
-      $('.gnb-pop').addClass('active')
-      // menu.play()
-    }
-
-  })
-
-  //메뉴 닫힘버튼
-  $('.btn-menu').click(function (e) {
-    e.preventDefault();
-    // $('.menu-wrap-mb').toggleClass('active');
-    $('.btn-menu').toggleClass('close');
+  $('body').toggleClass('hidden');
+  $('.btn-menu').toggleClass('close');
+  $('.gnb-pop').toggleClass('active')
+  // $('.header').toggleClass('menu-active');
 
 
 
 })
 
-  menu = gsap.to('.header .gnb-pop', {
-    opacity:1,
-    y:-800,
-    display: 'block',
-    paused: true
-  })
-
-      /**
-     * 메뉴 마우스 올리면 오퍼시티변경
-     *
-     * @version 1.0.0
-     * @since 2022-07-06
-     * @author jy
-     */
-
-  $('.gnb-item').hover(function(){
-    $(this).addClass('active');
-    $(this).siblings().removeClass('active');
-  })
-  $('.gnb-item').eq(0).trigger('hover')
 
 
-      /**
-     *동의 체크하면 체크,해제
-     *
-     * @version 1.0.0
-     * @since 2022-07-06
-     * @author jy
-     */
+
+// menu = gsap.to('.header .gnb-pop', {
+//   opacity:1,
+//   y:-800,
+//   display: 'block',
+//   paused: true
+// })
+
+  /**
+ * 메뉴 마우스 올리면 오퍼시티변경
+ *
+ * @version 1.0.0
+ * @since 2022-07-06
+ * @author jy
+ */
+
+$('.gnb-item').hover(function(){
+$(this).addClass('active');
+$(this).siblings().removeClass('active');
+})
+$('.gnb-item').eq(0).trigger('hover')
+
+
+  /**
+ *동의 체크하면 체크,해제
+ *
+ * @version 1.0.0
+ * @since 2022-07-06
+ * @author jy
+ */
 $('.agr-label').click(function(){
-  $('.agr-label').toggleClass('active')
+$('.agr-label').toggleClass('active')
 })
 
 
-    /**
-     * 패일리사이트에 마우스 올리면 메뉴나타나기
-     *
-     * @version 1.0.0
-     * @since 2022-07-06
-     * @author jy
-     */
+/**
+ * 패일리사이트에 마우스 올리면 메뉴나타나기
+ *
+ * @version 1.0.0
+ * @since 2022-07-06
+ * @author jy
+ */
 $('.family-wrap').hover(function () {
 $('.family-list').toggleClass('active');
 
@@ -210,126 +201,153 @@ $('.family-list').toggleClass('active');
 
 
 doctorsgroup =gsap.timeline({
-  scrollTrigger:{
-      trigger:'.sc-doctors-group',
-      start:"0% 80%",//트리거기준 0%위치, 윈도우기준0%
-      end:"100% 80%",
-      markers:true,
-      scrub:1,
-  }
+scrollTrigger:{
+  trigger:'.sc-doctors-group',
+  start:"0% 80%",//트리거기준 0%위치, 윈도우기준0%
+  end:"100% 80%",
+  // markers:true,
+  scrub:1,
+}
 })
 
 doctorsgroup.addLabel('motion1')
 
 .fromTo('.doctors-group-wrap.g01',{
-  y:-100,
-  opacity:0,
+y:-100,
+opacity:0,
 },{
-  y:50,
-  opacity:1,
+y:50,
+opacity:1,
 },'motion1')
 
 .fromTo('.doctors-group-wrap .desc-img',{
-  scale:1.3,
-  
+scale:1.3,
+
 },{
-  scale:1
+scale:1
 })
 .fromTo('.doctors-group-wrap.g02',{
-  y:-50,
-  opacity:0
+y:-50,
+opacity:0
 },{
-  y:100,
-  opacity:1
+y:100,
+opacity:1
 },'motion1')
 
 
 
 
-
-brand =gsap.timeline({
-  scrollTrigger:{
-      trigger:'.sc-brand-story',
-      start:"0% 80%",//트리거기준 0%위치, 윈도우기준0%
-      end:"70% 80%",
-      markers:true,
-      scrub:1,
-  }
-})
-
-brand.from('.video-area',{
-  x:-1000
+gsap.from('.video-area',{
+scrollTrigger:{
+  trigger:'.sc-brand-story',
+  start:"0% 60%",//트리거기준 0%위치, 윈도우기준0%
+  end:"70% 80%",
+  markers:true,
+  toggleActions:"play pause pause reset"
+},
+x:-1000
 })
 
 
 
 signature =gsap.timeline({
-  scrollTrigger:{
-    trigger:'.sc-signature',
-      start:"30% 80%",//트리거기준 0%위치, 윈도우기준0%
-      end:"100% 80%",
-      markers:true,
-      scrub:1,
-  }
+scrollTrigger:{
+trigger:'.sc-signature',
+  start:"30% 80%",//트리거기준 0%위치, 윈도우기준0%
+  end:"100% 80%",
+  // markers:true,
+  scrub:1,
+}
 })
 
 signature.addLabel('motion2')
 
 .fromTo('.sg-bg',{
-  opacity:0,
-  scale:0.3,
+opacity:0,
+scale:0.3,
 },{
-  opacity:1,
-  scale:1,
+opacity:1,
+scale:1,
 },'motion2')
 
 .fromTo('.sc-signature .signature-area::after',{
-  width:0,
-  
+width:0,
+
 },{
-  width:1000,
+width:1000,
 },'motion2')
 
 
-footer =gsap.timeline({
-  scrollTrigger:{
-    trigger:'.footer',
-      start:"0% 70%",//트리거기준 0%위치, 윈도우기준0%
-      end:"60% 70%",
-      markers:true,
-      scrub:1,
-  }
+
+
+
+
+// askBg = gsap.from('.sc-ask .bg',10,{
+//   scale:1.2,
+// })
+
+// ScrollTrigger.create({
+//   trigger:".sc-ask",
+//   start:"top 90%",
+//   end:"bottom top",
+//   markers:true,
+//   animation:askBg
+// })
+
+
+ScrollTrigger.create({
+trigger:".sc-ask",
+start:"top 90%",
+end:"bottom top",
+// markers:true,
+toggleClass: "active"
+// toggleClass: {targets: ".reserve-form", className: "active"}
 })
 
-footer.addLabel('motion3',{
-  stagger:0.5
-})
 
-.from('.corp',{
-  opacity:0,
-  y:-100
-},'motion3')
 
-.from('.reserve-form',{
-  opacity:0,
-  x:10
-},'motion3')
 
-.from('.contact-box.c01',{
-  opacity:0,
-  y:30,
-  stagger:0.3
-})
-.from('.contact-box.c02',{
-  opacity:0,
-  y:30,
-  stagger:0.3
-})
-.from('.contact-box.c03',{
-  opacity:0,
-  y:30,
-  stagger:0.3
-})
+
+
+// footer =gsap.timeline({
+//   scrollTrigger:{
+//     trigger:'.footer',
+//       start:"0% 70%",//트리거기준 0%위치, 윈도우기준0%
+//       end:"60% 70%",
+//       markers:true,
+//       scrub:1,
+//   }
+// })
+
+// footer.addLabel('motion3',{
+//   stagger:0.5
+// })
+
+// .from('.corp',{
+//   opacity:0,
+//   y:-100
+// },'motion3')
+
+// .from('.reserve-form',{
+//   opacity:0,
+//   x:10
+// },'motion3')
+
+// .from('.contact-box.c01',{
+//   opacity:0,
+//   y:30,
+//   stagger:0.3
+// })
+// .from('.contact-box.c02',{
+//   opacity:0,
+//   y:30,
+//   stagger:0.3
+// })
+// .from('.contact-box.c03',{
+//   opacity:0,
+//   y:30,
+//   stagger:0.3
+// })
 
 
 
@@ -338,7 +356,7 @@ footer.addLabel('motion3',{
 })
 
 function scrollRotate() {
-  let image = document.getElementById("btn-scroll");
+let image = document.getElementById("btn-scroll");
 image.style.transform = "rotate(" + window.pageYOffset/2 + "deg)";
 }
 
